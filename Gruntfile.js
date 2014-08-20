7f8b4ba65a1d5f71b900a30640761c0dac3eddd7
Shindeok Kang <shindeok.kang@dena.com>
2014-08-20 14:03:21 +0900

    grunt-contrib-watch と connect-livereload プラグインをインストールして LiveReload 設定

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,6 +4,7 @@ module.exports = function(grunt) {
 
   // 使用するプラグインを読み込み
   grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
   // タスクのオプション設定
   grunt.initConfig({
@@ -12,17 +13,36 @@ module.exports = function(grunt) {
       server: {
         options: {
           port: 9001,
-          base: 'app',
-          keepalive:true
+          middleware: function(connect) {
+            return [
+              require('connect-livereload')({ port: 35729 }),
+              connect.static(require('path').resolve('app'))
+            ]
+          }
         }
-      }
-    } // connect
+      } // connect:server
+    }, // connect
+
+    watch: {
+      livereload: {
+        options: {
+          livereload: 35729
+        },
+        files: [
+          'app/images/**/*.{png,jpg,gif}',
+          'app/styles/**/*.css',
+          'app/scripts/**/*.js',
+          'app/**/*.html'
+        ]
+      } // watch:livereload
+    } // watch
     
   });
 
   // プレビュータスク
   grunt.registerTask('serve', [
-    'connect'
+    'connect',
+    'watch'
   ]);
 
   // デフォルトのタスク
