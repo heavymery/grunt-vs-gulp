b553348c355c8502a9d0f7aa78dbbb58a61d68ef
Shindeok Kang <shindeok.kang@dena.com>
2014-08-20 14:45:12 +0900

    CSS と JS を Unify + Minify する簡単なビルドタスクを追加

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,9 +2,17 @@
 
 module.exports = function(grunt) {
 
-  // 使用するプラグインを読み込み
+  // 使用するプラグインを読み込み（プレビュー用）
   grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // 使用するプラグインを読み込み（ビルド用）
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-usemin');
 
   // タスクのオプション設定
   grunt.initConfig({
@@ -35,14 +43,62 @@ module.exports = function(grunt) {
           'app/**/*.html'
         ]
       } // watch:livereload
-    } // watch
-    
+    }, // watch
+
+    clean: {
+      dist: {
+        files: [{
+          dot: true,
+          src: ['dist']
+        }]
+      } // clean:dist
+    }, // clean
+
+    copy: {
+      build: {
+        files: [{
+          expand: true,
+          dot: true,
+          cwd: 'app',
+          dest: 'dist',
+          src: [
+            'images/**/*.{png,jpg,gif}',
+            '**/*.html'
+          ]
+        }]
+      } // copy:build
+    }, // copy
+
+    useminPrepare: {
+      html: 'app/index.html',
+      options: {
+        dest: 'dist'
+      }
+    }, // useminPrepare
+
+    usemin: {
+      html: [
+        'dist/**/*.html'
+      ] // usemin:html
+    } // usemin
+
   });
 
   // プレビュータスク
   grunt.registerTask('serve', [
     'connect',
     'watch'
+  ]);
+  
+  // ビルドタスク
+  grunt.registerTask('build', [
+    'clean:dist',
+    'copy:build',
+    'useminPrepare',
+    'concat',
+    'cssmin',
+    'uglify',
+    'usemin'
   ]);
 
   // デフォルトのタスク
