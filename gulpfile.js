d74f78a84ce837599583d711a7aab4485725bc7a
Shindeok Kang <shindeok.kang@dena.com>
2014-09-16 15:47:33 +0900

    gulp-connect プラグインを追加してプレビュー用タスクを設定

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,14 @@
 var gulp = require('gulp');
 
+var connect = require('gulp-connect');
+
+gulp.task('serve', function() {
+  connect.server({
+    root: 'app',
+    port: '9001'
+  });
+});
+
 gulp.task('default', function() {
   console.log('Grunt vs Gulp...');
 });
