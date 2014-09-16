b9148e4e7244f4ae51cc9aa9555ca542b566c0ae
Shindeok Kang <shindeok.kang@dena.com>
2014-09-16 15:57:13 +0900

    gulp-connect と gulp.watch でライブリロード設定

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,20 @@ var connect = require('gulp-connect');
 gulp.task('serve', function() {
   connect.server({
     root: 'app',
-    port: '9001'
+    port: '9001',
+    livereload: true
+  });
+  
+  gulp.watch(['./app/*.html'], function() {
+    gulp.src('./app/*.html').pipe(connect.reload());
+  });
+  
+  gulp.watch(['./app/styles/**/*.css'], function() {
+    gulp.src('./app/styles/**/*.css').pipe(connect.reload());
+  });
+  
+  gulp.watch(['./app/scripts/**/*.js'], function() {
+    gulp.src('./app/scripts/**/*.js').pipe(connect.reload());
   });
 });
 
