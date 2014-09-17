595f68424d713339722beec4c554217adcc3bad5
Shindeok Kang <shindeok.kang@dena.com>
2014-09-17 12:27:23 +0900

    ビルド用 gulp プラグインを追加してビルドタスク設定

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,19 @@
+'use strict';
+
 var gulp = require('gulp');
 
+// プレビュータスク用プラグイン
 var connect = require('gulp-connect');
 
+// ビルドタスク用プラグイン
+var usemin = require('gulp-usemin');
+var uglify = require('gulp-uglify');
+var minifyHtml = require('gulp-minify-html');
+var minifyCss = require('gulp-minify-css');
+var rev = require('gulp-rev');
+var clean = require('gulp-clean');
+
+// プレビュータスク
 gulp.task('serve', function() {
   connect.server({
     root: 'app',
@@ -22,6 +34,28 @@ gulp.task('serve', function() {
   });
 });
 
+// ビルド結果のプレビュータスク
+gulp.task('serve:dist', function() {
+  connect.server({
+    root: 'dist',
+    port: '9001'
+  });
+});
+
+// ビルドタスク
+gulp.task('build', function() {
+  gulp.src('dist/**/*', {read: false}).pipe(clean());
+    
+  gulp.src('./app/*.html')
+    .pipe(usemin({
+      css: [minifyCss(), 'concat', rev()],
+      html: [minifyHtml({empty: true})],
+      js: [uglify(), rev()]
+    }))
+    .pipe(gulp.dest('dist'));
+});
+
+// デフォルトタスク
 gulp.task('default', function() {
   console.log('Grunt vs Gulp...');
 });
